b85b7562a7a6817cd6a85e8ca389551a5e71427a
subinbabu89 <subin.babu@mavs.uta.edu>
2016-04-28 18:55:11 -0500

    fixed error

--- src/uta/mav/appoint/ScheduleAppointmentServlet.java
+++ src/uta/mav/appoint/ScheduleAppointmentServlet.java
@@ -2,10 +2,7 @@ package uta.mav.appoint;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Base64;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,54 +12,55 @@ import javax.servlet.http.HttpSession;
 import uta.mav.appoint.beans.Appointment;
 import uta.mav.appoint.beans.AppointmentType;
 import uta.mav.appoint.db.DatabaseManager;
-import uta.mav.appoint.login.LoginUser;
-import uta.mav.appoint.email.AppointmentEmail;
 import uta.mav.appoint.email.Email;
 import uta.mav.appoint.helpers.EncryptionDecryptionAES;
+import uta.mav.appoint.login.LoginUser;
 
-public class ScheduleAppointmentServlet extends HttpServlet{
+public class ScheduleAppointmentServlet extends HttpServlet {
 	private static final long serialVersionUID = -5925080374199613248L;
 	HttpSession session;
 	String header;
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		session = request.getSession();
-		LoginUser user = (LoginUser)session.getAttribute("user");
-		if (user == null){
+		LoginUser user = (LoginUser) session.getAttribute("user");
+		if (user == null) {
 			user = new LoginUser();
 		}
-		try{
+		try {
 			header = "templates/" + user.getHeader() + ".jsp";
 			int id = Integer.parseInt(request.getParameter("id1"));
-			ArrayList<TimeSlotComponent> array = (ArrayList<TimeSlotComponent>)session.getAttribute("schedules");
+			ArrayList<TimeSlotComponent> array = (ArrayList<TimeSlotComponent>) session.getAttribute("schedules");
 			DatabaseManager dbm = new DatabaseManager();
 			ArrayList<AppointmentType> ats = dbm.getAppointmentTypes(request.getParameter("pname"));
 			session.setAttribute("timeslot", array.get(id));
 			session.setAttribute("appointmenttypes", ats);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.printf(e.toString());
 		}
 		request.setAttribute("includeHeader", header);
 		request.getRequestDispatcher("/WEB-INF/jsp/views/schedule_appointment.jsp").forward(request, response);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		session = request.getSession();
-		try{
+		try {
 			Appointment a = new Appointment();
-			LoginUser user = (LoginUser)session.getAttribute("user");	
-			/*Hien*/
+			LoginUser user = (LoginUser) session.getAttribute("user");
+			/* Hien */
 			a.setStudentPhoneNumber(request.getParameter("phoneNumber"));
-			
-			String student_Id = request.getParameter("studentid");	
-			System.out.println("student_Id::: "+student_Id);
+
+			String student_Id = request.getParameter("studentid");
+			System.out.println("student_Id::: " + student_Id);
 			String encryptedStudentID = EncryptionDecryptionAES.encrypt(student_Id);
-			System.out.println("encryptedStudentID::: "+encryptedStudentID);
-						
+			System.out.println("encryptedStudentID::: " + encryptedStudentID);
+
 			a.setAppointmentId(Integer.parseInt(request.getParameter("id2")));
 			a.setStudentId(encryptedStudentID);
 			a.setDescription(request.getParameter("description"));
@@ -74,124 +72,122 @@ public class ScheduleAppointmentServlet extends HttpServlet{
 			a.setAdvisingDate(parts[3] + "-" + convertDate(parts[1]) + "-" + parts[2]);
 			parts = parts[4].split(":");
 			a.setAdvisingStartTime(parts[0] + ":" + parts[1]);
-			a.setAdvisingEndTime(addTime(parts[0],parts[1],d));
+			a.setAdvisingEndTime(addTime(parts[0], parts[1], d));
 			String email = request.getParameter("email");
 			DatabaseManager dbm = new DatabaseManager();
-			
+
 			String appDate = a.getAdvisingDate();
 			String sTime = a.getAdvisingStartTime();
 			String eTime = a.getAdvisingEndTime();
 			String stuEmail = request.getParameter("stuEmail");
-			String advEmail= request.getParameter("advisor_email");
-			
-			
-			
-			//Hien (right above dbm.create...)
-			System.out.println("Hien sent " +a.getStudentPhoneNumber());
-			
-			
-			
-			
-			
-			Boolean result = dbm.createAppointment(a,email);
+			String advEmail = request.getParameter("advisor_email");
+
+			// Hien (right above dbm.create...)
+			System.out.println("Hien sent " + a.getStudentPhoneNumber());
+
+			Boolean result = dbm.createAppointment(a, email);
 			System.out.println("before big if !!!!!!!!!!!!!!!!!...");
 
-			if (result == true){
-				response.setHeader("Refresh","2; URL=index");
-				request.getRequestDispatcher("/WEB-INF/jsp/views/success.jsp").forward(request,response);
+			if (result == true) {
+				response.setHeader("Refresh", "2; URL=index");
+				request.getRequestDispatcher("/WEB-INF/jsp/views/success.jsp").forward(request, response);
 				String sub = "Appointment set for " + a.getAdvisingDate();
-				String mess = ",\nAn appointment has been set for " + a.getAdvisingDate()
-				+ " at " + a.getAdvisingStartTime() + " - " + a.getAdvisingEndTime()
-				+ "\nAppoint ID: " + a.getAppointmentId();
-				Email newMail = new Email(sub,mess,email);
-				
-				
-/*				AppointmentEmail ae = new AppointmentEmail(appDate,sTime,eTime,stuEmail,advEmail);*/
-				
-				
-				
+				String mess = ",\nAn appointment has been set for " + a.getAdvisingDate() + " at "
+						+ a.getAdvisingStartTime() + " - " + a.getAdvisingEndTime() + "\nAppoint ID: "
+						+ a.getAppointmentId();
+				Email newMail = new Email(sub, mess, email);
+
+				/*
+				 * AppointmentEmail ae = new
+				 * AppointmentEmail(appDate,sTime,eTime,stuEmail,advEmail);
+				 */
+
 				System.out.println("before if !!!!!!!!!!!!!!!!!...");
-				
-				
+
 				DatabaseManager dbm2 = new DatabaseManager();
-				String emailnotification =dbm2.getNotification(user);
-				System.out.println("emailnotification in scheduleappnt ...    "+emailnotification);
+				String emailnotification = dbm2.getNotification(user);
+				System.out.println("emailnotification in scheduleappnt ...    " + emailnotification);
 				session.setAttribute("emailnotification", emailnotification);
-				
-				//String en = (String) session.getAttribute("emailnotification");
-				System.out.println("emailnotification error ...>>>>"+emailnotification);
-				if(emailnotification.equalsIgnoreCase("true")){
-				System.out.println("Sending email...");
-				newMail.sendMail();
-				
-				/*ae.sendEmail();*/
-				}
-				else
-				{
+
+				// String en = (String)
+				// session.getAttribute("emailnotification");
+				System.out.println("emailnotification error ...>>>>" + emailnotification);
+				if (emailnotification.equalsIgnoreCase("true")) {
+					System.out.println("Sending email...");
+					newMail.sendMail();
+
+					/* ae.sendEmail(); */
+				} else {
 					System.out.println("else in email...");
-					//newMail.sendMail();
+					// newMail.sendMail();
 				}
-			}
-			else{
+			} else {
 				System.out.println("in  else !!!!!!!!!!!!!!!!!...");
 
-				response.setHeader("Refresh","2; URL=advising");
-				request.getRequestDispatcher("/WEB-INF/jsp/views/failure.jsp").forward(request,response);
-			}		
-		}
-		catch(Exception e){
-			System.out.printf("EmailNotificationException "+e.toString());
+				response.setHeader("Refresh", "2; URL=advising");
+				request.getRequestDispatcher("/WEB-INF/jsp/views/failure.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			System.out.printf("EmailNotificationException " + e.toString());
 		}
 	}
-	
-	public String addTime(String hour, String minute, int add){
+
+	public String addTime(String hour, String minute, int add) {
 		String result = "";
-		try{
-		int h = Integer.parseInt(hour);
-		int m = Integer.parseInt(minute);
-		if (m + add >= 60){
-			m = m+add-60;
-			h++;
-		}
-		else{
-			m = m+add;
-		}
-		result = h+":"+m;
-		}
-		catch(Exception e){
-			
+		try {
+			int h = Integer.parseInt(hour);
+			int m = Integer.parseInt(minute);
+			if (m + add >= 60) {
+				m = m + add - 60;
+				h++;
+			} else {
+				m = m + add;
+			}
+			result = h + ":" + m;
+		} catch (Exception e) {
+
 		}
 		return result;
 	}
-	
-	public String convertDate(String d){
-		if (d.equals("Jan")){
+
+	public String convertDate(String d) {
+		if (d.equals("Jan")) {
 			return "1";
-		}if (d.equals("Feb")){
+		}
+		if (d.equals("Feb")) {
 			return "2";
-		}if (d.equals("Mar")){
+		}
+		if (d.equals("Mar")) {
 			return "3";
-		}if (d.equals("Apr")){
+		}
+		if (d.equals("Apr")) {
 			return "4";
-		}if (d.equals("May")){
+		}
+		if (d.equals("May")) {
 			return "5";
-		}if (d.equals("Jun")){
+		}
+		if (d.equals("Jun")) {
 			return "6";
-		}if (d.equals("Jul")){
+		}
+		if (d.equals("Jul")) {
 			return "7";
-		}if (d.equals("Aug")){
+		}
+		if (d.equals("Aug")) {
 			return "8";
-		}if (d.equals("Sep")){
+		}
+		if (d.equals("Sep")) {
 			return "9";
-		}if (d.equals("Oct")){
+		}
+		if (d.equals("Oct")) {
 			return "10";
-		}if (d.equals("Nov")){
+		}
+		if (d.equals("Nov")) {
 			return "11";
-		}if (d.equals("Dec")){
+		}
+		if (d.equals("Dec")) {
 			return "12";
 		}
 		return null;
 	}
 
 }
-
